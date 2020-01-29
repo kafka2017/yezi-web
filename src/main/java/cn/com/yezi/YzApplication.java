b447d65b56f5b3a7d891987c1d1d3cf44201777a
wxf <260008747@qq.com>
2020-01-29 18:17:28 +0800

    优化jar

--- src/main/java/cn/com/yezi/YzApplication.java
+++ src/main/java/cn/com/yezi/YzApplication.java
@@ -3,16 +3,22 @@ package cn.com.yezi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ServletComponentScan
 @ComponentScan(basePackages = {"cn.com.yezi"})
-
-public class YzApplication {
+public class YzApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
     	SpringApplication.run(YzApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(YzApplication.class);
     }
 }
